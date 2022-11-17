22d292c99f94adf217414c1885be89c37b54a4a7
Mr-Dey <106720486+Mr-Dey@users.noreply.github.com>
2022-11-17 08:13:25 +0530

    updated

--- FORM VALDATION ASSINGMENT/error_handeler_script.js
+++ FORM VALDATION ASSINGMENT/error_handeler_script.js
@@ -1,21 +1,26 @@
 function seterror(id, error) {
     var element = document.getElementById(id);
     element.getElementsByClassName("formError")[0].innerHTML = error;
-
 }
 
+
 function validationForm() {
-    var returnvalue = true;
-    var name = document.forms["myForm"]["f_name"].value;
-    if (isNaN(name)){
-        null;
-    }
-    else{
-        seterror("stu_name", "Please enter your name.");//this is id and error message!
-    }
-
+    var returnval = true;
+    var f_name = document.getElementById('f_name').value;
+    var l_name = document.getElementById('l_name').value;
+    var mof_name = document.getElementById('mof_name').value;
+    var mol_name = document.getElementById('mol_name').value;
+    var faf_name = document.getElementById('faf_name').value;
+    var fal_name = document.getElementById('fal_name').value;
     
-    // addEventListener("click",function(event){event.preventDefault()});
-    // return returnvalue;
+    if (f_name.length >15 || isNaN(f_name)==false) {
+        seterror("stu_name","Please enter a valid name.");
+        console.log(f_name);
+        returnval=false;    
+    }
 
+
+
+
+  return returnval;  
 }
